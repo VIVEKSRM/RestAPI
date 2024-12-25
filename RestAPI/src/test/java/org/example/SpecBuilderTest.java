7bacb2ca99c418b0886fa16c5f4ebf0379e856ff
VIVEKSRM <vivek.ranjan.srm@gmail.com>
2024-12-25 14:56:31 +0530

    Spec builder AND ResponseSpecification Examples

--- RestAPI/src/test/java/org/example/SpecBuilderTest.java
+++ RestAPI/src/test/java/org/example/SpecBuilderTest.java
@@ -4,13 +4,17 @@ import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import pojo.AddPlace;
 import pojo.Location;
+import reUsableMethods.commonMethod;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.responseSpecification;
+import static org.hamcrest.Matchers.equalTo;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,20 +42,46 @@ public class SpecBuilderTest {
 		l.setLng(33.427362);
 		p.setLocation(l);
 
-		RequestSpecification req =new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addQueryParam("key", "qaclick123")
-				.setContentType(ContentType.JSON).build();
+		RequestSpecification req =new RequestSpecBuilder()
+				.setBaseUri("https://rahulshettyacademy.com")
+				.addQueryParam("key", "qaclick123")
+				.setContentType(ContentType.JSON)
+				.build();
 
+		ResponseSpecification resspec =new ResponseSpecBuilder()
+				.expectStatusCode(200)
+				.expectContentType(ContentType.JSON)
+				.build();
 
-
-		ResponseSpecification resspec =new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
 		RequestSpecification res=given().spec(req)
 				.body(p);
 
 		Response response =res.when().post("/maps/api/place/add/json").
 				then().spec(resspec).extract().response();
 
+		//Below is the direct Example
+/*		Response response =given().spec(req).body(p)
+				.when().post("/maps/api/place/add/json")
+				.then().spec(resspec)
+				.extract().response();*/
+
 		String responseString=response.asString();
 		System.out.println(responseString);
+		JsonPath js = commonMethod.getJson(responseString);
+		String placeID=js.getString("place_id");
+
+		RequestSpecification res1=given().queryParam("place_id", placeID);
+		Response responce1=res1.get();
+		System.out.println("responce1 :-" +responce1.asString());
+
+/*		String expectedNewAddress="101 Summer walk USA";
+		String getPlace=given().log().all().queryParam("key","qaclick123")
+				.queryParam("place_id", Place_Id.get("IdOne"))
+				.when().get("maps/api/place/get/json")
+				.then().log().all()
+				.assertThat().statusCode(200)
+				//verification of New address from RestAPI method
+				.body("address",equalTo("101 Summer walk USA")).extract().response().asString();*/
 	}
 
 }
